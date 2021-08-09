628af2049923c9e573df64fc2103ec73f661271d
Garrett <garrettodom97@gmail.com>
2021-08-09 16:25:18 -0500

    fixes typo; post to patch

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get "/employees" => "employees#index"
   get "/employees/:id" => "employees#show"
   post "employees" => "employees#create"
-  post "employees/:id" => "employees#update"
+  patch "employees/:id" => "employees#update"
   delete "employees/:id" => "employees#destroy"
 
   # routes for department index and department show actions
